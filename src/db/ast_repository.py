4076e8049feb052d8c8988ac347594c8f2cc6b01
Hiroaki Mori <96053873+hiroakinsm@users.noreply.github.com>
2025-01-31 11:45:30 +0900

    Delete src directory

--- src/db/ast_repository.py
+++
@@ -1,7 +0,0 @@
-class ASTRepository:
-    def __init__(self, connection_string):
-        self.connection_string = connection_string
-
-    def get_ast(self, program_id):
-        # 実装
-        pass 
